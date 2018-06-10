7638dd21b8d58a9128848b4c442799ee42bb600a
prescott breeden <prescott.breeden@gmail.com>
2018-06-09 21:40:04 -0700

    training set and test set
    answers in both python and R

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -7,39 +7,42 @@ dataset = read.csv('Salary_Data.csv')
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Salary, SplitRatio = 2/3)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+split <- sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
 
 # Fitting Simple Linear Regression to the Training set
-regressor = lm(formula = Salary ~ YearsExperience,
-               data = training_set)
+regressor <- lm(formula = Salary ~ YearsExperience,
+                data = training_set)
+
+summary(regressor)
 
 # Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set)
+y_pred <- predict(regressor, newdata = test_set)
 
-# Visualising the Training set results
+# Visualizing the Training set results
 library(ggplot2)
 ggplot() +
-  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
-             colour = 'red') +
+  geom_point(aes(x = training_set$YearsExperience, training_set$Salary),
+             color = 'red') +
   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Training set)') +
-  xlab('Years of experience') +
+            color = 'blue') +
+  ggtitle('Salary vs Years Experience (Training Set)') +
+  xlab('Years of Experience') +
   ylab('Salary')
 
-# Visualising the Test set results
-library(ggplot2)
+# Visualizing the Test set results
 ggplot() +
-  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
-             colour = 'red') +
+  geom_point(aes(x = test_set$YearsExperience, test_set$Salary),
+             color = 'red') +
   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Test set)') +
-  xlab('Years of experience') +
+            color = 'blue') +
+  ggtitle('Salary vs Years Experience (Test Set)') +
+  xlab('Years of Experience') +
   ylab('Salary')
+
+
+
+
+
+#
--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -11,8 +11,9 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+                                    X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -22,7 +23,7 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Simple Linear Regression to the Training set
+# Fitting Simple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
@@ -30,18 +31,22 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
-# Visualising the Training set results
+# Visualizing the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
+plt.title('Salary vs Experience (Training Set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Test set results
+# Visualizing the Test set results
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
+plt.title('Salary vs Experience (Test Set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+
+
+#
